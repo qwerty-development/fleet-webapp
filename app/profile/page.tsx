94876aa3d1b1ad94668196e3f0480ca1a1f6548c
RRTDKS <ryan.alam001@gmail.com>
2025-03-20 15:31:46 +0200

    img to Image

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -319,7 +319,7 @@ export default function ProfilePage() {
               {/* Avatar and Upload Button */}
               <div className="relative inline-block mb-6">
                 <div className="h-32 w-32 rounded-full overflow-hidden border-4 border-white/20 bg-gray-800">
-                  <Image
+                  <img
                     src={avatarUrl}
                     alt="Profile"
                     width={128}
